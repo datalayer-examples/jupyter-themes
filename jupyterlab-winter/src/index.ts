bdde3fb61b830698e3fa6950ed2398374c98e175
Eric Charles <eric@datalayer.io>
2023-08-28 07:19:25 +0200

    lint: log message

--- jupyterlab-winter/src/index.ts
+++ jupyterlab-winter/src/index.ts
@@ -9,7 +9,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
   requires: [IThemeManager],
   autoStart: true,
   activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
-    console.log('JupyterLab extension @datalayer/jupyter-theme-winter is activated!');
+    console.log('JupyterLab plugin @datalayer/jupyter-theme-winter is activated!');
     const style = '@datalayer/jupyter-theme-winter/index.css';
     manager.register({
       name: 'Winter',
